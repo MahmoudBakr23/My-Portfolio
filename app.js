e30baa4a512e73bbb4ed446c284dc450b3a0b53b
Mahmoud Bakr <mbakr6821@gmail.com>
2020-09-30 01:27:06 +0200

    get burger button works

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
+const openNavModal = document.querySelectorAll('[data-modal-nav]')
+const closeNavModal = document.querySelectorAll('[data-close-nav]')
+
+openNavModal.forEach(b => {
+    b.addEventListener('click', () => {
+        const nav = document.querySelector(b.dataset.modalNav)
+        openNav(nav)
+    })
+})
+
+closeNavModal.forEach(b => {
+    b.addEventListener('click', () => {
+        const nav = b.closest('#nav-modal')
+        closeNav(nav)
+    })
+})
+
+function openNav(nav) {
+    if (nav == null) return
+    nav.classList.add('active')
+}
+
+function closeNav(nav) {
+    if (nav == null) return
+    nav.classList.remove('active')
+}
 
 openModalButtons.forEach(button => {
     button.addEventListener('click', () => {
